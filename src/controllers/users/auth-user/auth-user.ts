f26b529b8d39523ba1a26b34d822aa86f500001c
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-04-22 17:04:23 -0300

    feat: new HttpSatusCode and controller will return unauthorized

--- src/controllers/users/auth-user/auth-user.ts
+++ src/controllers/users/auth-user/auth-user.ts
@@ -1,7 +1,7 @@
 import { IAuthUserService } from "../../../services/auth-user/protocols";
 import { IGetUserByEmailRepository } from "../../../services/get-user-by-email/protocols";
 import { HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest, serverError } from "../../utils";
+import { badRequest, serverError, unauthorized } from "../../utils";
 import {
   AuthUserParams,
   AuthUserResponse,
@@ -35,18 +35,14 @@ export class AuthUserController {
       const authUser = await this.authUserService.authUser(email, password);
 
       if (!authUser) {
-        return badRequest("Invalid credentials");
+        return unauthorized("Invalid credentials");
       }
 
       return {
         statusCode: 200,
-        body: {
-          email: authUser.email,
-          token: authUser.token,
-        },
+        body: authUser,
       };
     } catch (error) {
-      console.error(error);
       return serverError();
     }
   }
