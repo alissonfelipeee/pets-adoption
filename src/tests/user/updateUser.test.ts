f75474a93f7aad3f725bd1198a299bff20dcad82
Alisson Felipe <alissonfelipenettt@gmail.com>
2023-04-23 11:26:33 -0300

    test: added new tests in updateUser and fix others

--- src/tests/user/updateUser.test.ts
+++ src/tests/user/updateUser.test.ts
@@ -1,9 +1,14 @@
+import { AuthUserController } from "../../controllers/users/auth-user/auth-user";
+import { CreateUserController } from "../../controllers/users/create-user/create-user";
 import { User } from "../../models/User";
+import { AuthUserService } from "../../services/auth-user/auth-user";
 import { UpdateUserController } from "./../../controllers/users/update-user/update-user";
-import { InMemoryUserRepository } from "./repositories/in-memory";
+import {
+  InMemoryGetUserByEmailRepository,
+  InMemoryUserRepository,
+} from "./repositories/in-memory";
 
-const userExample = {
-  id: 1,
+const user = {
   firstName: "John",
   lastName: "Doe",
   email: "johndoe@gmail.com",
@@ -11,31 +16,63 @@ const userExample = {
   phone: "(61) 90000-0000",
 } as User;
 
+let token: string;
+
 describe("Update User", () => {
+  beforeAll(async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const inMemoryGetUserByEmailRepository =
+      new InMemoryGetUserByEmailRepository();
+    const createUserController = new CreateUserController(
+      inMemoryUserRepository,
+      inMemoryGetUserByEmailRepository
+    );
+
+    await createUserController.handle({
+      body: user,
+    });
+
+    const authUserService = new AuthUserService(
+      inMemoryGetUserByEmailRepository
+    );
+    const authUserController = new AuthUserController(authUserService);
+
+    const { statusCode, body } = await authUserController.handle({
+      body: {
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    const newbody = JSON.stringify(body);
+    token = JSON.parse(newbody).token;
+  });
+
   it("should update user", async () => {
     const inMemoryUserRepository = new InMemoryUserRepository();
     const updateUserController = new UpdateUserController(
       inMemoryUserRepository
     );
 
-    const userCreated = await inMemoryUserRepository.createUser(userExample);
-
-    const user = await updateUserController.handle({
+    const { statusCode, body } = await updateUserController.handle({
       params: {
-        id: userCreated.id,
+        id: 1,
       },
       body: {
         lastName: "Doe Jr",
       },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     });
 
-    const { password, ...userWithoutPassword } = user.body as User;
+    const { password, ...userWithoutPassword } = body as User;
 
-    expect(user.body).toEqual({
+    expect(body).toEqual({
       ...userWithoutPassword,
       lastName: "Doe Jr",
     });
-    expect(user.statusCode).toBe(200);
+    expect(statusCode).toBe(200);
   });
 
   it("should update user and update password", async () => {
@@ -44,25 +81,26 @@ describe("Update User", () => {
       inMemoryUserRepository
     );
 
-    const userCreated = await inMemoryUserRepository.createUser(userExample);
-
-    const user = await updateUserController.handle({
+    const { statusCode, body } = await updateUserController.handle({
       params: {
-        id: userCreated.id,
+        id: 1,
       },
       body: {
         lastName: "Doe Jr",
         password: "1234567",
       },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     });
 
-    const { password, ...userWithoutPassword } = user.body as User;
+    const { password, ...userWithoutPassword } = body as User;
 
-    expect(user.body).toEqual({
+    expect(body).toEqual({
       ...userWithoutPassword,
       lastName: "Doe Jr",
     });
-    expect(user.statusCode).toBe(200);
+    expect(statusCode).toBe(200);
   });
 
   it("should return 400 if id is not provided", async () => {
@@ -71,15 +109,38 @@ describe("Update User", () => {
       inMemoryUserRepository
     );
 
-    const user = await updateUserController.handle({
+    const { statusCode, body } = await updateUserController.handle({
       params: {},
       body: {
         lastName: "Doe Jr",
       },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     });
 
-    expect(user.body).toBe("Bad Request - Missing param: id");
-    expect(user.statusCode).toBe(400);
+    expect(body).toBe("Bad Request - Missing param: id");
+    expect(statusCode).toBe(400);
+  });
+
+  it("should return error because missing authorization", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const { statusCode, body } = await updateUserController.handle({
+      params: {
+        id: 1,
+      },
+      body: {
+        lastName: "Doe Jr",
+      },
+      headers: {},
+    });
+
+    expect(body).toBe("Bad Request - Missing header: authorization");
+    expect(statusCode).toBe(400);
   });
 
   it("should return 400 if body is not provided", async () => {
@@ -88,14 +149,63 @@ describe("Update User", () => {
       inMemoryUserRepository
     );
 
-    const user = await updateUserController.handle({
+    const { statusCode, body } = await updateUserController.handle({
       params: {
         id: 1,
       },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     });
 
-    expect(user.body).toBe("Bad Request - Missing body");
-    expect(user.statusCode).toBe(400);
+    expect(body).toBe("Bad Request - Missing body");
+    expect(statusCode).toBe(400);
+  });
+
+  it("should return error because token is invalid", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    const { statusCode, body } = await updateUserController.handle({
+      params: {
+        id: 1,
+      },
+      body: {
+        lastName: "Doe Jr",
+      },
+      headers: {
+        authorization: `Bearer ${token}123`,
+      },
+    });
+
+    expect(body).toBe("Unauthorized - Invalid token");
+    expect(statusCode).toBe(401);
+  });
+
+  it("should return error because token invalid for this user", async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const updateUserController = new UpdateUserController(
+      inMemoryUserRepository
+    );
+
+    await inMemoryUserRepository.createUser(user);
+
+    const { statusCode, body } = await updateUserController.handle({
+      params: {
+        id: 2,
+      },
+      body: {
+        lastName: "Doe Jr",
+      },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+
+    expect(body).toBe("Unauthorized - Invalid token for this user");
+    expect(statusCode).toBe(401);
   });
 
   it("should return 400 if there are invalid fields", async () => {
@@ -104,17 +214,20 @@ describe("Update User", () => {
       inMemoryUserRepository
     ) as any; // I found no other solution to perform this test other than changing the controller type to ANY, so that the error occurs!
 
-    const user = await updateUserController.handle({
+    const {statusCode, body} = await updateUserController.handle({
       params: {
         id: 1,
       },
       body: {
         email: "johndoe2@gmail.com",
       },
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
     });
 
-    expect(user.body).toBe("Bad Request - Invalid fields");
-    expect(user.statusCode).toBe(400);
+    expect(body).toBe("Bad Request - Invalid fields");
+    expect(statusCode).toBe(400);
   });
 
   it("should return 500 if something goes wrong", async () => {
